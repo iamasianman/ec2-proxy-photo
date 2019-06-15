1086472a4dae447e4e4eb4349aac53b5b3b4f101
iamasianman <kevinle1991@gmail.com>
2019-06-15 10:32:44 -0700

    proxy up

---
+++ script/script.js
@@ -0,0 +1,17 @@
+import http from "k6/http";
+import { check, sleep } from "k6";
+export let options = {
+  vus: 1000,
+  duration: "3m",
+  rps: 1000,
+};
+
+export default function () {
+  let id = Math.floor(Math.random() * 10000000);
+  let res = http.get(`http://localhost:3000/api/restaurants/${id}/photos`);
+  check(res, {
+    // eslint-disable-next-line eqeqeq
+    "success": (r) => r.status == 200
+  });
+  sleep(2);
+}
--- server/server.js
+++ server/server.js
@@ -1,21 +1,40 @@
+require('newrelic');
 const express = require('express');
 const port = 3000;
 const path = require('path');
 const cors = require('cors');
 const app = express();
+const axios = require('axios');
 
 app.use(cors());
 app.use(express.static(path.resolve(__dirname, '../client/public')));
 
-app.get('/:id', (req, res) => {
-  if (!req.params.id) {
-    res.status(400);
-    res.end();
-  } else {
-    res.sendFile('index.html', { root: path.resolve(__dirname, '../client/public') });
-  }
+app.get('/restaurants/:id/photos', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/public/index.html'));
+});
+
+app.get('/api/restaurants/:id/photos', (req, res) => {
+  const { id } = req.params;
+  axios.get(`http://localhost:3060/api/restaurants/${id}/photos`)
+    .then(response => {
+      res.status(200).send(response.data);
+    })
+    .catch(err => {
+      res.status(500).send();
+    });
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}!`));
 
 module.exports.app = app;
+
+
+
+// app.get('/:id', (req, res) => {
+//     if (!req.params.id) {
+//       res.status(400);
+//       res.end();
+//     } else {
+//       res.sendFile('index.html', { root: path.resolve(__dirname, '../client/public') });
+//     }
+//   });
